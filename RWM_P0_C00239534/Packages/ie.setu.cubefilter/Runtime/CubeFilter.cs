dd7aaf98e12a7fd483cefc1dfee5361a7783c615
unknown <c0023934@itcarlow.ie>
2022-09-12 13:08:36 +0100

    added first thing filrter

---
+++ RWM_P0_C00239534/Packages/ie.setu.cubefilter/Runtime/CubeFilter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFilter
+{
+  
+
+    public static int[] DecrementIfLessThanFirstPass(int[] xs)
+    {
+        int[] result = new int[xs.Length];
+
+        int firstNum = 0;
+        int check = 0;
+        for (int i = 0; i < xs.Length; i++)
+        {
+            result[i] = xs[i];
+
+            if (check == 0)
+            {
+                firstNum = xs[i];
+            }
+            check = 1;
+
+
+            if (firstNum > xs[i])
+            {
+                result[i] -= 1;
+            }
+
+        }
+        return result;
+    }
+}
---
+++ RWM_P0_C00239534/Packages/ie.setu.cubefilter/Tests/CubeFilterTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 10, 9, 12, 5, 5, 999 };
+            int[] output = CubeFilter.DecrementIfLessThanFirstPass(input);
+            int[] expected = { 10, 8, 12, 4, 4, 999 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
