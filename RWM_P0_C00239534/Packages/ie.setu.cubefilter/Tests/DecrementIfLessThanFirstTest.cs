2dd76b3352f28aa79d6c699addb784b4f9ec8adf
David Jalisevs <58332574+DavidJalisevs@users.noreply.github.com>
2022-09-19 09:26:17 +0100

    changed names and merged

---
+++ RWM_P0_C00239534/Packages/ie.setu.cubefilter/Tests/DecrementIfLessThanFirstTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class DecrementIfLessThanFirstTest
+    {
+        [Test]
+        public void DecrementIfLessThanFirstSimple()
+        {
+            int[] input = { 10, 9, 12, 5, 5, 999 };
+            int[] output = DecrementIfLessThanFirst.DecrementIfLessThanFirstPass(input);
+            int[] expected = { 10, 8, 12, 4, 4, 999 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+
+
+        [Test]
+        public void Module3Simple()
+        {
+            int[] input = { 10, 9, 12, 5, 5, 8 };
+            int[] output = CubeFilter.Module3(input);
+            int[] expected = { 1, 0, 0, 2, 2, 2 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+
+
+
+
+
+
+    }
+}
